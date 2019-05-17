b12608ff4b5c81cee5f499124aef44d2ad05d7a0
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2019-05-17 22:10:41 +0700

    Implemented slack verification and add the tests

--- example/slash_app.py
+++ example/slash_app.py
@@ -1,41 +1,31 @@
+import os
+
 from flask import Flask,request
 
+from slacksdk.verify import verify
 
 app = Flask(__name__)
 
-
+SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
 
 @app.route("/callback", methods=["POST"])
 def hello_slack():
-    """Example Incoming Data - application/x-www-form-urlencoded
-    token=gIkuvaNzQIHg97ATvDxqgjtO
-    team_id=T0001
-    team_domain=example
-    enterprise_id=E0001
-    enterprise_name=Globular%20Construct%20Inc
-    channel_id=C2147483705
-    channel_name=test
-    user_id=U2147483697
-    user_name=Steve
-    command=/weather
-    text=94070
-    response_url=https://hooks.slack.com/commands/1234/5678
-    trigger_id=13345224609.738474920.8088930838d88f008e0
-    """
-    print(request.form)
-    return f"""For every Data Scientists, let's express your abilities into the world of software development.
+    if verify(SLACK_SIGNING_SECRET,request):
+        return f"""For every Data Scientists, let's express your abilities into the world of software development.
 You will learn many things including.\n
     - Advanced Python Topics\n
         - Object Oriented Programing\n
         - Python Decorators\n
         - Meta Programing\n
-    - Bacnkend Web API\n
+    - Backend Web API\n
     - Documentation\n
     - Unit Testing\n
-    - CI/CI\n
+    - CI/CD\n
     - Security\n
 Join this group and have fun!
 """
+    else:
+        return "You are not verified"
 
 if __name__ == "__main__":
     app.run()
